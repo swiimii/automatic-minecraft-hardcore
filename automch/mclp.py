21ce944822e2baabeb5c5672c6aa380dd89f201e
Samuel Scherer <swiimii962@gmail.com>
2021-02-22 17:57:20 -0500

    Second try to detect deaths

--- automch/mclp.py
+++ automch/mclp.py
@@ -2,7 +2,15 @@
 import re, datetime, operator, os, time
 from deathcauses import causes as deathcauseslist
 
-deathsListRegString = '('+'|'.join(deathcauseslist)+')'
+def restart_server():
+    # os.system(r"kill -9 ps aux | grep -i java | grep -v grep | awk '{print $2}'")
+    # os.system(r"rm -r ./world")
+    # os.system(r"rm -r ./logs")
+    # os.system(r"java -Xmx1024M -Xms1024M -jar server.jar nogui")
+    print("MUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\n")
+    time.sleep(5)
+
+deathsListRegString = r'^\[([0-2][0-9]):([0-9]{2}):([0-9]{2})\] \[Server Thread/INFO\]: ('+'|'.join(deathcauseslist)+')'
 deathregex = re.compile(deathsListRegString)
 # Read log file as it's being written. Thanks stackoverflow
 def follow(thefile):
@@ -13,21 +21,13 @@ def follow(thefile):
             time.sleep(0.1) # Sleep briefly
             continue
         yield line
-
+list
 
 while(True):
-    path = open('logs/latest.log')
+    path = open('./logs/latest.log')
     for line in follow(path):
-        if deathregex.match(line):
+        print(line)
+        if deathregex.search(line):
             restart_server()
             break
     time.sleep(.1)
-
-def restart_server():
-    # os.system(r"kill -9 ps aux | grep -i java | grep -v grep | awk '{print $2}'")
-    # os.system(r"rm -r ./world")
-    # os.system(r"rm -r ./logs")
-    # os.system(r"java -Xmx1024M -Xms1024M -jar server.jar nogui")
-    print("MUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\nMUCH DEAD VERY WOW\n")
-    time.sleep(5)
-
